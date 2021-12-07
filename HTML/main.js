7b2076cc2300883255e0980ca7dda299030e0a73
Jemil2301 <jemil.jethva2001@gmail.com>
2021-12-07 15:02:20 +0530

    adding 2 Faq page

--- HTML/main.js
+++ HTML/main.js
@@ -34,5 +34,17 @@ $(document).ready(function() {
 });
 
 
+const details = document.querySelectorAll("details");
 
+// Add the onclick listeners.
+details.forEach((targetDetail) => {
+  targetDetail.addEventListener("click", () => {
+    // Close all the details that are not targetDetail.
+    details.forEach((detail) => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
 
