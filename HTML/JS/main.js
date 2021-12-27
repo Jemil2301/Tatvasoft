0fe6292a2c38ca3e110f0926ec2e9679c96c8aba
Jemil2301 <jemil.jethva2001@gmail.com>
2021-12-27 09:07:25 +0530

    add 8th page

--- HTML/JS/main.js
+++ HTML/JS/main.js
@@ -60,3 +60,77 @@ details.forEach((targetDetail) => {
 });
 
 
+$(document).ready(function() {
+  var countrow=$(".datatable tbody tr").length;
+  $("#numbertr").text("Total Record:"+countrow);
+  
+  $(".rateYo").rateYo({
+    rating: 4,
+    starWidth: "17px",
+    spacing: "2px",
+    ratedFill:"#ecb91c",
+    fullStar: true,
+    precision: 2,
+    normalFill:"#d4d4d4",
+
+
+
+});
+   $(".rateYo").rateYo().on("rateyo.change", function (e, data) {
+    var rating = data.rating;
+    $(this).parent().find('.ratingnum').text(rating);
+
+   });
+
+
+});
+
+
+$(document).ready(function() {
+    $('#table-id').DataTable({
+        "bFilter": false,
+        "lengthMenu": [[5,10, 25, 50, -1], [5,10, 25, 50, "All"]],
+        "dom":"tlip",
+         'dom': "<'row'<'col-sm-12'tr>>" +
+         "<'row'<'col-sm-3'l>,<'col-sm-4'i>,<'col-sm-4'p>>",
+          "pagingType": "full_numbers",
+          "oLanguage": {
+"sInfo": "Total Records: _TOTAL_"
+}, 'language': {
+ 'oPaginate': {
+   'sNext': '<i class="fa fa-chevron-right"></i>',
+   'sPrevious': '<i class="fa fa-chevron-left"></i>',
+   'sFirst': '<i class="fa fa-step-backward"></i>',
+   'sLast': '<i class="fa fa-step-forward"></i>',
+
+   }
+   } ,
+ 
+   "drawCallback": function( settings ) {
+        $("#table-id").wrap( "<div class='table-responsive'></div>" );
+    },
+    'aoColumnDefs': [{
+        'bSortable': false,
+        'aTargets': [-1] /* 1st one, start by the right */
+    }]
+
+     
+
+    });
+  
+   
+
+
+} );
+
+
+
+
+
+
+
+
+
+
+
+
